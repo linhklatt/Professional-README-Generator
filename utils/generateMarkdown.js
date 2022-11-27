a8ffbbb732c9b467d389c42e1dae6c1d430cb05d
Linh Klatt <linhklatt@Linhs-MacBook-Pro.local>
2022-11-26 21:39:56 -0800

    deleted unnessesary codes and added code for Usage/License/Contributing/Test sections

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,59 +1,59 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(answers) {
   return `
-  ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
   
   
-  # ${answers.projectTitle}
-  ${answers.license} 
+# ${answers.projectTitle}
+${answers.license} 
 
 
-  ## Description
-  ${answers.description}
+## Description
+${answers.description}
 
-  ## Table of Contents
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [License](#license)
-  * [Contributing](#contributing)
-  * [Test](#test)
-  * [Questions](#question)
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Test](#test)
+* [Questions](#question)
 
 
-  ## Installation
-  To install nescessary dependencies, run the following command
-  \`\`\`
-  ${answers.iDependencies}
-  \`\`\`
+## Installation
+To install nescessary dependencies, run the following command
+\`\`\`
+${answers.iDependencies}
+\`\`\`
  
 
-  ## Usage
-  ${answers.usage}
+## Usage
+${answers.guide}
 
-  ## License
+## License
+${answers.license} 
 
+## Contributing
+${answers.UserContribution} 
 
-  ## Contributing
-
-
-  ## Test
-
+## Test
+${answers.testsCommand}
 
 
   ## Question
-  If you have anyquestions about the repo, open and issue or contact me directly at linhklatt@yahoo.com. You can find more of my works at [linhklatt](email)
+  If you have anyquestions about the repo, open and issue or contact me directly at ${answers.email}. You can find more of my works at [linhklatt](email)
 
 `;
 }
